d8933c059a0f4e0c95ffc82947800073ea0ec95a
Rohit <rohitpandhare9@gmail.com>
2023-01-29 12:14:00 +0530

    new file of java

---
+++ rohit_pandhare/src/logical/message/Office.java
@@ -0,0 +1,20 @@
+package logical.message;
+
+public class Office {
+
+	String name;
+	String ceoName;
+    Address addr2;
+    
+    public Address infoAdd() {
+    	Address a = new Address();
+    	return a;
+    }
+    
+    public void infoMsg(EmployeeInfo obj1) {
+    	
+    	obj1.showMessage("hello");
+    	
+    	
+    }
+}
