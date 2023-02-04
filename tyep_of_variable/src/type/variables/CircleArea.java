3d98d8249dac76157e17ab9b6b1e033cea98ac12
right way learning <rwl@gmail.com>
2023-02-04 21:32:48 +0530

    type of viable

---
+++ tyep_of_variable/src/type/variables/CircleArea.java
@@ -0,0 +1,14 @@
+package type.variables;
+
+public class CircleArea {
+	
+	public float pi;
+	public float reduis;
+	
+	
+	public float getCircleArea() {
+		float area = pi*reduis*reduis;
+		return area;
+	}
+
+}
