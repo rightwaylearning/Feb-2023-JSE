549de6a33b9e1e1d8a2a92021cf099a8e576866e
Your Name <you@example.com>
2023-02-04 22:00:59 +0530

    Create a files

---
+++ Student_info/src/first/TeacherReference.java
@@ -0,0 +1,16 @@
+package first;
+
+public class TeacherReference {
+	
+	Student generateStudent(String name,int num) {
+		
+		Student s= new Student();
+		s.studentName= "Prachi";
+		s.rollNumber=2200;
+		return s;
+		
+		
+	}
+	
+	
+}
