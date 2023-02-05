fb6df5f0b5e18b078ea8bb45f0e6ae8d26f924aa
babarsonali821@gmail.com <babarsonali821@gmail.com>
2023-02-05 19:27:44 +0530

    data

--- Sonali_Babar/src/circle/EntryPoint.java
+++ Sonali_Babar/src/circle/EntryPoint.java
@@ -4,9 +4,12 @@ public class EntryPoint
 {
 	public static void main(String[] arg)
 	{
-		CircleArea c=new CircleArea();
+		CircleAreaMethod c=new CircleAreaMethod();
 		c.pi=3.14f;
 		c.radius=2.5f;
+		
+		float f=c.getCircleAreaMethod();
+		System.out.println(f);
 		
 	}
 	
