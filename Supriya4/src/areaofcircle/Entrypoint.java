9e0ad2887af29ea4505d090307d87b7ad09c8365
supriya <supriya@gmail.com>
2023-02-06 14:12:08 +0530

    data

---
+++ Supriya4/src/areaofcircle/Entrypoint.java
@@ -0,0 +1,13 @@
+package areaofcircle;
+
+public class Entrypoint {
+	public static void main(String[] args) {
+		CircleArea c=new CircleArea();
+	    c.radius=3.4f;
+	    CircleArea.pi=3.14f;
+	     
+	    float r=c.getCircleArea();
+	    System.out.println(r);
+	}
+
+}
