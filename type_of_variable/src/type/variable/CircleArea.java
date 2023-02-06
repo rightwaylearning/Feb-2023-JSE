7655627466d68779c01a296af98cb9473a7dadca
Jyoti kendre <jyotikendre1999@gmail.com>
2023-02-06 22:56:21 +0530

    data

---
+++ type_of_variable/src/type/variable/CircleArea.java
@@ -0,0 +1,16 @@
+package type.variable;
+
+public class CircleArea {
+	
+	public float pi;
+	public float redius;
+	
+	public float getCircleArea() {
+		
+		float area = pi*redius*redius;
+		return area;
+	}
+	
+	
+
+}
