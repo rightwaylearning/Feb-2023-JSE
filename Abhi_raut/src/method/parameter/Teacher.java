1ff750f6b4bde3f122e122e045a223d1773275af
abhi982355 <rautabhishek2002@gmail.com>
2023-02-07 11:56:44 +0530

    modified method

--- Abhi_raut/src/method/parameter/Teacher.java
+++ Abhi_raut/src/method/parameter/Teacher.java
@@ -5,7 +5,7 @@ public class Teacher {
 	Student teacherinfo( String name,int num,long no) {
 		
 	Student s = new Student();
-	s.name="name";
+	s.name="Rohit";
 	s.rollNumber=num;
 	s.mobileNo=no;
 	
