4b171fc4a95a9b730039d1eaa2b1bcf129c14189
abhi982355 <rautabhishek2002@gmail.com>
2023-02-08 11:55:54 +0530

    use this

---
+++ Abhi_raut/src/this_method/Test.java
@@ -0,0 +1,18 @@
+package this_method;
+
+public class Test {
+     
+	int a;
+	int b;
+	
+ public void m1 () {
+	 
+	 int a = 250;
+	 
+	 System.out.println(a);
+	 System.out.println(b);
+	 
+	 System.out.println(this a);
+     System.out.println(this b);	 
+ }
+}
