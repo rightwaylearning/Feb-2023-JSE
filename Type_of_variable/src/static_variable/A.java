665c75b2ee91889c581a4b64245c7b05ef8bdfce
supriya <supriya@gmail.com>
2023-02-08 15:50:54 +0530

    data

--- Type_of_variable/src/static_variable/A.java
+++ Type_of_variable/src/static_variable/A.java
@@ -1,5 +1,7 @@
 package static_variable;
 
 public class A {
-
+	int x;
+	int y;
+	static int z;
 }
--- Type_of_variable/src/static_variable/Entrypoint.java
+++ Type_of_variable/src/static_variable/Entrypoint.java
@@ -1,5 +1,34 @@
 package static_variable;
 
+
 public class Entrypoint {
+	public static void main(String[] args) {
+		
+		A a=new A();
+		a.x=10;
+		a.y=20;
+		
+		A a1=new A();
+		a1.x=34;
+		a1.y=12;
+		
+		A s= new A();
+		s.x =10;
+		s.y =20;
+		A.z =100000; 
+		
+		A s1= new A();
+		s1.x =1;
+		s1.y =2;
+		A.z =999;
+		
+		A a2= new A();
+		a2.x = 100;
+		a2.y = 200;
+		
+		System.out.println(A.z);
+		System.out.println(A.z);
+		
+	}
 
 }
