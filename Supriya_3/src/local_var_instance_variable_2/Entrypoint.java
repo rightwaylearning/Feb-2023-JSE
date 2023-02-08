51cc02fa0e75c27834306a15f42b80d1ac803b4f
supriya <supriya@gmail.com>
2023-02-08 21:10:24 +0530

    data

---
+++ Supriya_3/src/local_var_instance_variable_2/Entrypoint.java
@@ -0,0 +1,12 @@
+package local_var_instance_variable_2;
+
+public class Entrypoint {
+
+	public static void main(String[] args) {
+		Test t1 =new Test();
+		     t1.a=1;
+		     t1.b=2;
+		     t1.m1();
+		
+	}
+}
