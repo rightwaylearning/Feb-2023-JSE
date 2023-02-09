37aaaafe9c048b834744c55597d3442fe1c63a8c
komal <chaudharikomal146@gmail.com>
2023-02-09 18:42:53 +0530

    java program

---
+++ komal_chaudhari/src/type/variable_static/EntryPoint.java
@@ -0,0 +1,14 @@
+package type.variable_static;
+
+public class EntryPoint {
+	
+	public static void main(String[] args) {
+		
+		
+		Circle c1 = new circle
+		c1.radius = 5.6F;
+		
+		c1.getCircle
+	}
+
+}
