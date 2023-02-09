89e252d711c40a9936b3deba649cab4ccef0e289
Your Name <you@example.com>
2023-02-09 20:11:03 +0530

    create the class

---
+++ Prachi_Pawar/src/this_reference_example/Tester.java
@@ -0,0 +1,15 @@
+package this_reference_example;
+
+public class Tester {
+	
+public static void main(String[] args) {
+		
+		Test obj =new Test();
+		obj.a = 10;
+		obj.b =20;
+		
+		obj.m1();
+		
+	}
+
+}
