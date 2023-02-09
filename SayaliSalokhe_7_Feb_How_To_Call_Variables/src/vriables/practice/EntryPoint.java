10a0d38f981a05ec92bb60256350adbe55d5036e
chavan <sayalichavan288@gmail.com>
2023-02-09 13:33:16 +0530

    send java 8-2-23 practice assingment

--- SayaliSalokhe_7_Feb_How_To_Call_Variables/src/vriables/practice/EntryPoint.java
+++ SayaliSalokhe_7_Feb_How_To_Call_Variables/src/vriables/practice/EntryPoint.java
@@ -1,5 +1,10 @@
 package vriables.practice;
 
 public class EntryPoint {
-
+ public static void main(String[] args) {
+	 Variables obj=new Variables();
+	 obj.a=99;
+	 obj.b=999;
+	 obj.m1(88);
+}
 }
