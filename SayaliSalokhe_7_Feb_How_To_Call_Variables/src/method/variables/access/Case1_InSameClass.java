410f5bd95b26db6429a631967fc297509cfb93bc
chavan <sayalichavan288@gmail.com>
2023-02-10 17:56:00 +0530

    send  java assingment static and instance varible access in same class

---
+++ SayaliSalokhe_7_Feb_How_To_Call_Variables/src/method/variables/access/Case1_InSameClass.java
@@ -0,0 +1,26 @@
+package method.variables.access;
+
+public class Case1_InSameClass {
+	 int a;   //instance variable
+	 int b;   //instance variable
+	static int x;   //static variable
+	static int y;  //static variable
+	
+	void m1()//instance/non static method
+	{
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(x);
+		System.out.println(y);
+	}
+	static void m2()//static method
+	{
+		Case1_InSameClass c1=new Case1_InSameClass();//create object for access instance variable
+		System.out.println(c1.a);
+		System.out.println(c1.b);
+		System.out.println(Case1_InSameClass.x); //best practice to access static varable using class name
+		System.out.println(Case1_InSameClass.y);
+		
+	}
+
+}
---
+++ tyep_of_variable/src/methods_variable_access/Case1.java
@@ -0,0 +1,29 @@
+package methods_variable_access;
+
+public class Case1 {
+	
+	int x;
+	int y;
+	static int m;
+	static int n;
+	
+	
+	void m1() {
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(m); // best practice for static A.m
+		System.out.println(n); // A.n		
+	}
+	
+	static void m2() {
+		
+		Case1  c=new Case1();
+		System.out.println(c.x);
+		System.out.println(c.y);
+		
+		System.out.println(m); // A.m
+		System.out.println(n); // A.n		
+		
+	}
+
+}
