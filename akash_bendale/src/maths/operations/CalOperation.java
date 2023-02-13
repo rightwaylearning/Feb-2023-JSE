d8ba72d8de4b3057d582f999e85ebd0d5bc0f4ab
Akash Bendale <akashbendale@gmail.com>
2023-02-13 21:54:13 +0530

    data

---
+++ akash_bendale/src/maths/operations/CalOperation.java
@@ -0,0 +1,12 @@
+package maths.operations;
+
+public class CalOperation {
+	int a=10;
+	int b=14;
+	
+	int performAddition() {
+		int resultofAddition=this.int a + this.b;
+		return resultofAddition;
+		
+	}
+}
