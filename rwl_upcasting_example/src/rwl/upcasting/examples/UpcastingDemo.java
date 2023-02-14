80a0cd6194e949fbe636330e5294ff0f206bfb96
right way learning <java.development.pune@gmail.com>
2023-02-14 08:32:40 +0530

    data

---
+++ rwl_upcasting_example/src/rwl/upcasting/examples/UpcastingDemo.java
@@ -0,0 +1,23 @@
+package rwl.upcasting.examples;
+
+public class UpcastingDemo {
+
+	public static void main(String[] args) {
+		
+		 char ch ='A';  // 65
+		 
+		 double d = ch;
+		 
+		 System.out.println(d);
+		 
+		 
+		 short s = 1; 
+		 
+	//	 byte b = s; // type mismatch: cannot convert from short to byte
+		 
+		 
+	}
+	
+	 
+	
+}
