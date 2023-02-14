889ae374eb345eae5e65461e2869b0ac01cfbba8
Rohit <rohitpandhare9@gmail.com>
2023-02-14 14:44:28 +0530

    upcasting

---
+++ rohit_pandhare/src/upcasting/pack/Case1.java
@@ -0,0 +1,27 @@
+package upcasting.pack;
+
+public class Case1 {
+	
+	public static void main(String[] args) {
+		
+		int i = 4242;
+		long l = i;
+		
+		long l1 = 323L;
+		
+		float  f = l1;
+		double d = l1;
+		
+		System.out.println(f);
+		System.out.println(d);
+		
+		char ch = 'a';
+		int a = ch;
+		char ch1 = 'z';
+		int a1 = ch1;
+		
+		System.out.println(a);
+		System.out.println(a1);
+	}
+
+}
