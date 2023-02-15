bec815bb8cd8fdca590eb2c48c0ee4340d4154d3
Your Name <you@example.com>
2023-02-15 23:24:19 +0530

    create a flow and control package

---
+++ operators_and_flow_control_pp/src/Arithmatic_Operation/Main.java
@@ -0,0 +1,24 @@
+package Arithmatic_Operation;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int a=35;
+		int b=65;
+		
+		System.out.println(a+b);
+		
+		System.out.println(a-b);
+		
+		System.out.println(a*b);
+		
+		System.out.println(a/b);
+		
+		System.out.println(a%b);
+		
+		
+	}
+	
+
+}
