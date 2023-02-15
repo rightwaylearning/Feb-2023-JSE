b0cc72b7366a21e76172d5de63da665895c612fa
Jyoti kendre <jyotikendre1999@gmail.com>
2023-02-15 21:39:16 +0530

    Data

---
+++ operators_and_flow_controls_jkk/src/arithmatic/operator/Main.java
@@ -0,0 +1,19 @@
+package arithmatic.operator;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int a = 25;
+		int b = 35;
+		
+		System.out.println(a+b);
+		
+		System.out.println(a-b);
+		
+		System.out.println(a*b);
+		
+		System.out.println(a%b);
+	}
+
+}
