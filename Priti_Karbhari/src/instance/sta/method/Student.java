ec0eed4d20f08756b67bceabb001286bdc668514
priti <pkarbhari0399@gmail.com>
2023-02-16 19:54:09 +0530

    practice

---
+++ Priti_Karbhari/src/instance/sta/method/Student.java
@@ -0,0 +1,13 @@
+package instance.sta.method;
+
+public class Student {
+	void m1() {
+  	  
+  	  System.out.println("this is m1 method (instance method)");
+    }
+static void m2() {
+	  
+	  System.out.println("this is m2 method (static method)");
+}
+
+}
