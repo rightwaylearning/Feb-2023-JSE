23b157422179a3117800fd38b0670548b052e83a
gitanjali <gitanjalishirke1998@gmail.com>
2023-02-17 20:00:28 +0530

    data

---
+++ Gitanjali_relational_opt/src/relational_opt/Relational_opt.java
@@ -0,0 +1,12 @@
+package relational_opt;
+
+public class Relational_opt {
+	public static void main(String[] args) {
+		double d = 8;
+		double d1 = 7;
+		System.out.println(d1 > d);
+		System.out.println(d <= d1);
+		System.out.println(d + d1);
+		System.out.println(d > d1);
+	}
+}
