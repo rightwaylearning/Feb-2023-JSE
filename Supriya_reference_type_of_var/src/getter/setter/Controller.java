f7ca1682ec2619e0009bf685db7f695a58ea0856
supriya <supriya@gmail.com>
2023-02-17 22:18:24 +0530

    insert files

--- Supriya_reference_type_of_var/src/getter/setter/Controller.java
+++ Supriya_reference_type_of_var/src/getter/setter/Controller.java
@@ -1,9 +1,35 @@
 package getter.setter;
 
+
 public class Controller {
 
 	public static void main(String[] args) {
-		 Student s=new Student();
+		
+Student s = new Student();
+		
+		s.setRollNumber(23);
+		s.setsName("Raghu");
+		
+		
+		//int rollNumber = s.getRollNumber();
+			
+		System.out.println(s.getRollNumber());
+		
+		//String name = s.getsName();
+		System.out.println(s.getsName());
+		
+				
+		
+		
+		
+		
+		
+		 //Student s=new Student();
+		 //s.getRollNumber();
+		 s.getsName();
+		  
+		 //System.out.println(getRollNumber(123));
+		 
 		 
 	}
 }
--- Supriya_reference_type_of_var/src/getter/setter/Student.java
+++ Supriya_reference_type_of_var/src/getter/setter/Student.java
@@ -1,18 +1,22 @@
 package getter.setter;
 
 public class Student {
-	
+
 	private String sName;
 	private int rollNumber;
+
 	public String getsName() {
 		return sName;
 	}
+
 	public void setsName(String sName) {
 		this.sName = sName;
 	}
+
 	public int getRollNumber() {
 		return rollNumber;
 	}
+
 	public void setRollNumber(int rollNumber) {
 		this.rollNumber = rollNumber;
 	}
