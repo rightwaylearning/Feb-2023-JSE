d389847237de68e4ffe6b9bd67b3ebe94bf439d7
shinde umesh <shindeu108@gmail.com>
2023-02-19 17:13:51 +0700

    new data

---
+++ shindeumesh/src/setter_getter_mthod/controller.java
@@ -0,0 +1,21 @@
+package setter_getter_mthod;
+
+public class controller {
+
+	public static void main(String[] args) {
+		
+		Student S = new Student();
+	S.setName("ramu");
+		S.setRollNumber(21);
+	S.setSchoolName("MG");
+	
+	}
+	
+
+	
+
+	
+
+	}
+	
+
