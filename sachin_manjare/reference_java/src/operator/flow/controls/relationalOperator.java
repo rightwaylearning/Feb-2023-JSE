2614432f68db6c80ba8af21d0df351b0b17de186
sachin manjare <sachinmanjare93@gmail.com>
2023-02-19 22:05:43 +0530

    new program

---
+++ sachin_manjare/reference_java/src/operator/flow/controls/relationalOperator.java
@@ -0,0 +1,18 @@
+package operator.flow.controls;
+
+public class relationalOperator {
+	
+	  int a;
+	  int b;
+	
+	public static void main(String[]args) {
+		
+		 relationalOperator r= new  relationalOperator();
+		 
+		
+	    boolean r = a<b;
+	
+	     System.out.println(r);
+	
+	}
+}
---
+++ sachin_manjare/reference_java/src/set/get/method3/studentImformation.java
@@ -0,0 +1,31 @@
+package set.get.method3;
+
+public class studentImformation {
+	
+	 private String studentName;
+	 private int rollNumber;
+	
+	 
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public int getRollNumber() {
+		return rollNumber;
+	}
+	public void setRollNumber(int rollNumber) {
+		this.rollNumber = rollNumber;
+	}
+
+	 
+		
+		
+		
+		
+		
+	}
+	
+
+
