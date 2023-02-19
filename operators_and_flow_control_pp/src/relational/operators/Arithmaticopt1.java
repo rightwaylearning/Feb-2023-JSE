995327523204efd19c35e83434484f592863e3d9
Your Name <you@example.com>
2023-02-20 00:46:13 +0530

    create a classes

---
+++ operators_and_flow_control_pp/src/relational/operators/Arithmaticopt1.java
@@ -0,0 +1,25 @@
+package relational.operators;
+
+public class Arithmaticopt1 {
+	
+	public static void main(String[] args) {
+		
+		byte b=10;
+		byte a= 20;
+		
+		int r= a+b; // max(int,byte,int)=int
+		
+		short s=45;
+		short s1= 45;
+		
+		int r1= s+s1;
+		
+		System.out.println(25/2.0);
+		
+		int r11= 'A'*2;
+		
+		System.out.println(r11);
+		
+	}
+
+}
