bc50084f1378d746a9aca482231b3768db4232b9
JayantiRokade <rokadejaynti7101@gmail.com>
2023-02-19 21:10:40 +0530

    programs

---
+++ jayanti_type_of_variables/src/type/variables/CircleArea.java
@@ -0,0 +1,16 @@
+package type.variables;
+
+public class CircleArea {
+	
+	public float pi;
+	public float redius;
+	
+	public float getCircleArea() {
+		float area = pi*pi*redius;
+		return area;
+		
+		
+		
+	}
+
+}
