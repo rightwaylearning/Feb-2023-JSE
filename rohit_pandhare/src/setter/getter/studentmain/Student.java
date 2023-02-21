85881784ecb32671a8aebe3b0a57a983b8dcc5cc
Rohit <rohitpandhare9@gmail.com>
2023-02-21 10:46:56 +0530

    practice setter getter method

--- rohit_pandhare/src/setter/getter/studentmain/Student.java
+++ rohit_pandhare/src/setter/getter/studentmain/Student.java
@@ -1,5 +1,35 @@
 package setter.getter.studentmain;
 
 public class Student {
+	
+	private int rollNo;
+	private String name;
+	private Address adr;
+	
+	public void setAdr(Address adr) {
+		this.adr = adr;
+		
+	}
+	
+	public Address getAdr() {
+		return adr;
+		
+	}
+
+	public int getRollNo() {
+		return rollNo;
+	}
+
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
