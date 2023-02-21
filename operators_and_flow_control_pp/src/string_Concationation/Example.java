8173c6f4aff1afbc684f6ee2d253eb030d6ca24f
Your Name <you@example.com>
2023-02-21 23:55:07 +0530

    setter getter methods

--- operators_and_flow_control_pp/src/string_Concationation/Example.java
+++ operators_and_flow_control_pp/src/string_Concationation/Example.java
@@ -23,7 +23,25 @@ public class Example {
 		
 		System.out.println("Student name= "+ name);
 		System.out.println("Student age= "+ age);
+	
+	//-----------------------------------------------
 		
+		String str = 10 + 20 + "10" + 20 + 90;
+	    System.out.println("final result "+ str);
+		
+	    //-----------------------------------------
+	    System.out.println(6+6+6+6); // 24
+	    System.out.println("6"+6+6+6); // "6666"
+	    System.out.println(6+6+6+"6"); // "186"
+	    System.out.println(6+6+"6"+6); // "1266"
+	    
+	    int a = 10;
+	    int b = 20;
+	    String c= "30";
+	    String  r22 = a + b +c;    
+        System.out.println(r22);
+        
+        System.out.println("" + 10 +true+10.23+'A');
 		
 		
 		
