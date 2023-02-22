306308e53340b06815c5428f67928ce30baf04e0
Rohit <rohitpandhare9@gmail.com>
2023-02-22 11:54:45 +0530

    constructor with object parameter

--- rohit_pandhare/src/constructor/use/typeofmethod/EntryPointStudent.java
+++ rohit_pandhare/src/constructor/use/typeofmethod/EntryPointStudent.java
@@ -7,13 +7,22 @@ public class EntryPointStudent {
 //		constructor nahi ghetla tr value assign karav lagel
 //		fkt etkach difference aahe
 		
-		Student s = new Student(32,"rahul");
+		Student s = new Student(32,"rahul",new Address());
 		s.getName();
+		
+		                  Address adr = new Address();
+		                  adr.setLine1(544444);
+		s.setAdr(adr);
 		
 		s.setName("vijay");
 		s.getRollNo();
 		
 		s.setName("digvijay");
+		
+		Address adr2= s.getAdr();
+		adr2.getLine1();
+		
+		System.out.println(adr2.getLine1());
 		
 		System.out.println(s.getName());
 		System.out.println(s.getRollNo());
