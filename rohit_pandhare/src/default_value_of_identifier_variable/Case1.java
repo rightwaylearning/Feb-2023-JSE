e3ec4820c1a18e6c20bc3d194a4f9d0594762dc2
Rohit <rohitpandhare9@gmail.com>
2023-02-23 20:20:07 +0530

    constructor

--- rohit_pandhare/src/default_value_of_identifier_variable/Case1.java
+++ rohit_pandhare/src/default_value_of_identifier_variable/Case1.java
@@ -17,5 +17,67 @@ public class Case1 {
 	
 	String str;
 	Address adr;
+	public byte getB() {
+		return b;
+	}
+	public void setB(byte b) {
+		this.b = b;
+	}
+	public short getS() {
+		return s;
+	}
+	public void setS(short s) {
+		this.s = s;
+	}
+	public int getI() {
+		return i;
+	}
+	public void setI(int i) {
+		this.i = i;
+	}
+	public long getL() {
+		return l;
+	}
+	public void setL(long l) {
+		this.l = l;
+	}
+	public float getF() {
+		return f;
+	}
+	public void setF(float f) {
+		this.f = f;
+	}
+	public double getD() {
+		return d;
+	}
+	public void setD(double d) {
+		this.d = d;
+	}
+	public char getC() {
+		return c;
+	}
+	public void setC(char c) {
+		this.c = c;
+	}
+	public boolean isB1() {
+		return b1;
+	}
+	public void setB1(boolean b1) {
+		this.b1 = b1;
+	}
+	public String getStr() {
+		return str;
+	}
+	public void setStr(String str) {
+		this.str = str;
+	}
+	public Address getAdr() {
+		return adr;
+	}
+	public void setAdr(Address adr) {
+		this.adr = adr;
+	}
+	
+	
 
 }
--- rohit_pandhare/src/default_value_of_identifier_variable/EntryPointCase1.java
+++ rohit_pandhare/src/default_value_of_identifier_variable/EntryPointCase1.java
@@ -4,6 +4,11 @@ public class EntryPointCase1 {
 	
 	public static void main(String[] args) {
 		
+//		suppose default value kadhyachi aasel tr parameterized
+//		constructor ghyachach nahi
+		
+//		parameterized constructor mhnje to initialize value 
+//		when we create object 
 		int a ;
 //		System.out.println(a);
 //		aapn aasa karu nahi shakt karan local var la bydefault 
@@ -12,19 +17,31 @@ public class EntryPointCase1 {
 		
 		Case1 c =new Case1();
 		
-		System.out.println(c.b);
-		System.out.println(c.s);
-		System.out.println(c.i);
-		System.out.println(c.l);
-		System.out.println(c.f); //primitive all default values are
-		System.out.println(c.d); //0 
-		                          //and only char showing empty box
-		System.out.println(c.c);  //and boolean is false
-		System.out.println(c.b1);
-	
-//		this are ref type
-		System.out.println(c.str);
-		System.out.println(c.adr);
+//		aata aasa lihycha nahi aata constructor madhe intialize karaych
+		
+//		System.out.println(c.b);
+//		System.out.println(c.s);
+//		System.out.println(c.i);
+//		System.out.println(c.l);
+//		System.out.println(c.f); //primitive all default values are
+//		System.out.println(c.d); //0 
+//		                          //and only char showing empty box
+//		System.out.println(c.c);  //and boolean is false
+//		System.out.println(c.b1);
+//	
+////		this are ref type
+//		System.out.println(c.str);
+//		System.out.println(c.adr);
+		
+		System.out.println(c.getB());
+		System.out.println(c.getS());
+		System.out.println(c.getI());
+		System.out.println(c.getL());
+		System.out.println(c.getF());
+		System.out.println(c.getD());
+		System.out.println("default char value =  "+c.getC());
+		System.out.println(c.isB1());
+		
 	
 		System.out.println("........................");
 		
@@ -34,7 +51,7 @@ public class EntryPointCase1 {
 		System.out.println(Case2.l);
 		System.out.println(Case2.f); 
 		System.out.println(Case2.d);  
-		System.out.println(Case2.c);
+		System.out.println("default char value in static is ' ' empty space =  "+Case2.c);
 		System.out.println(Case2.b1);
 	
 //		this are ref type
