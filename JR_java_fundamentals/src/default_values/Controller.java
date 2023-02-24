f153aabe4ff6330b346de6896f3c617895e19425
JayantiRokade <rokadejaynti7101@gmail.com>
2023-02-24 12:44:18 +0530

    programs

---
+++ JR_java_fundamentals/src/default_values/Controller.java
@@ -0,0 +1,22 @@
+package default_values;
+
+public class Controller {
+	
+	public static void main(String[] args) {
+		
+		
+		Test t= new Test();
+		
+		System.out.println("byte="+t.getb());
+		System.out.println("byte="+t.getb());
+		System.out.println("byte="+t.getb());
+		System.out.println("byte="+t.getb());
+	
+		System.out.println("byte="+t.getb());
+		System.out.println("byte="+t.getb());
+		
+		System.out.println("byte="+t.getb());
+		System.out.println("byte="+t.getb());
+	}
+
+}
---
+++ JR_java_fundamentals/src/default_values/Test.java
@@ -0,0 +1,7 @@
+package default_values;
+
+public class Test {
+	
+	
+
+}
