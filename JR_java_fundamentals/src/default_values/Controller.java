4281aeac0e095623b191b36e5b93200bbb81086d
JayantiRokade <rokadejaynti7101@gmail.com>
2023-02-24 16:33:44 +0530

    programs

--- JR_java_fundamentals/src/default_values/Controller.java
+++ JR_java_fundamentals/src/default_values/Controller.java
@@ -7,16 +7,20 @@ public class Controller {
 		
 		Test t= new Test();
 		
-		System.out.println("byte="+t.getb());
-		System.out.println("byte="+t.getb());
-		System.out.println("byte="+t.getb());
-		System.out.println("byte="+t.getb());
-	
-		System.out.println("byte="+t.getb());
-		System.out.println("byte="+t.getb());
+		System.out.println("byte ="+t.getB());
+		System.out.println("short ="+t.getS());
+		System.out.println("int="+t.getI());
+		System.out.println("long ="+t.getL());
 		
-		System.out.println("byte="+t.getb());
-		System.out.println("byte="+t.getb());
+		System.out.println("float ="+t.getF());
+		System.out.println("double ="+t.getD());
+		
+		System.out.println("char ="+t.getC());
+		System.out.println("boolean="+t.getB1());
+		
+		System.out.print("String str="+t.getStr());
+		
+		
 	}
 
 }
