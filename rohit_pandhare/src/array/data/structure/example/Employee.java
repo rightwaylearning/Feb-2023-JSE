565b9c08d9428cb64044f0bfe885eb2a872088b5
Rohit <rohitpandhare9@gmail.com>
2023-02-25 14:48:58 +0530

    array data structure

---
+++ rohit_pandhare/src/array/data/structure/example/Employee.java
@@ -0,0 +1,36 @@
+package array.data.structure.example;
+
+public class Employee {
+	
+	private int empId;
+	private String empName;
+	
+	public Employee(int empId, String empName) {
+		
+		this.empId = empId;
+		this.empName = empName;
+	}
+	
+	public Employee(){}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	
+	
+	
+
+}
---
+++ rohit_pandhare/src/array/data/structure/example/EntrypointToStroreArrayObject.java
@@ -0,0 +1,49 @@
+package array.data.structure.example;
+
+public class EntrypointToStroreArrayObject {
+
+	public static void main(String[] args) {
+		
+		Employee[] e = new Employee[4];
+		
+//		3 is the size of array index then 0 to 2
+		                            
+		                           
+		Employee e1 = new Employee(32,"Ravi");
+				
+		e[0]= e1;
+		
+		Employee e2 = new Employee();
+		e2.setEmpId(33);
+		e2.setEmpName("Rahul");
+		
+		e[1]= e2;
+	
+		System.out.println(">>>>>>>>>>>>>>>>>>>");
+		
+		e[2]= new Employee();
+		e[2].setEmpId(34);
+		e[2].setEmpName("Riyansh");
+		
+		System.out.println("................");
+
+		e[3]= new Employee(35,"prem");
+		
+		System.out.println("<<<<<<<<Print Data>>>>>");
+		
+		System.out.println(e[0].getEmpId());
+		System.out.println(e[0].getEmpName());
+		
+		System.out.println("<<<<<<<<Print Data using loop>>>>>");
+	int i = 0;
+	
+		while(i < e.length ) {
+			
+			System.out.println("Employee name = "+e[i].getEmpName());
+			System.out.println("Employee name = "+e[i].getEmpId());
+			
+			System.out.println(">>>>>>>>>>>>>>>>>>>");
+			i++;
+		}
+	}
+}
