128043af44ae9f95ceecaccc2baef5e12d5f5e58
tejas.birdawade <tejasbirdawade1234@gmail.com>
2023-02-26 17:20:43 +0530

    setter and getter

---
+++ Tejas_Birdawade/src/newPackage/controller.java
@@ -0,0 +1,16 @@
+package newPackage;
+
+public class controller {
+
+	 public static void main(String[] args) {
+		int a =  10;
+		int b = a--;
+		System.out.println(b);
+  
+		int a1 = 10;
+		a1--;
+		System.out.println(a1);
+		
+	 }
+	
+}
