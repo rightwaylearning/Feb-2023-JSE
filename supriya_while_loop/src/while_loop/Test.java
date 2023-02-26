9968a5ffc9ce222c1ab8b9c079c181a3be5bd9b8
supriya <supriya@gmail.com>
2023-02-26 23:39:03 +0530

    data

---
+++ supriya_while_loop/src/while_loop/Test.java
@@ -0,0 +1,14 @@
+package while_loop;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println(">>>>>>>>>>>>>>>>>Start");
+		int i=10;
+		while(i<=20) {
+			System.out.println(i);
+			i++;
+			//System.out.println(">>>>>>>>>>>>>>>end");
+		}
+	}
+
+}
