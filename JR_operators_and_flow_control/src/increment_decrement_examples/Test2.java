4adf38ef3f97b3f39f18a6c27c985fc21bc60cf6
JayantiRokade <rokadejaynti7101@gmail.com>
2023-02-28 19:25:05 +0530

    programs

---
+++ JR_operators_and_flow_control/src/increment_decrement_examples/Test2.java
@@ -0,0 +1,17 @@
+package increment_decrement_examples;
+
+public class Test2 {
+	
+	public static void main(String[] args) {
+		int p=100;
+		int q=200;
+		
+		System.out.println("Value of p++: "+(p++));
+		System.out.println("Value of q++: "+q++);
+		
+		System.out.println("Value of p++: "+p++);
+		System.out.println("Value of q++: "+q++);
+		
+	}
+
+}
