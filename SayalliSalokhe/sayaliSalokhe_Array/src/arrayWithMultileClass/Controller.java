3b3c87ba3ab7f3f9c6add005541578fdec082da2
sayali salokhe <sayalisalokhe895@gmail.com>
2023-02-28 08:58:52 +0530

    send  array assingment

---
+++ SayalliSalokhe/sayaliSalokhe_Array/src/arrayWithMultileClass/Controller.java
@@ -0,0 +1,11 @@
+package arrayWithMultileClass;
+
+public class Controller {
+	public static void main(String[] args) {
+		Student student = new Student();
+		student.setName("sayali");
+		student.setRollNo(1);
+		student.;
+	}
+
+}
---
+++ SayalliSalokhe/sayaliSalokhe_Array/src/arrayWithMultileClass/Student.java
@@ -0,0 +1,64 @@
+package arrayWithMultileClass;
+
+public class Student {
+	private int rollNo;
+	private String name;
+	private Address[] adr;
+	private int[] marks;
+	private School school;
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(int rollNo, String name, Address[] adr, int[] marks, School school) {
+		super();
+		this.rollNo = rollNo;
+		this.name = name;
+		this.adr = adr;
+		this.marks = marks;
+		this.school = school;
+	}
+
+	public int getRollNo() {
+		return rollNo;
+	}
+
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Address[] getAdr() {
+		return adr;
+	}
+
+	public void setAdr(Address[] adr) {
+		this.adr = adr;
+	}
+
+	public int[] getMarks() {
+		return marks;
+	}
+
+	public void setMarks(int[] marks) {
+		this.marks = marks;
+	}
+
+	public School getSchool() {
+		return school;
+	}
+
+	public void setSchool(School school) {
+		this.school = school;
+	}
+
+}
