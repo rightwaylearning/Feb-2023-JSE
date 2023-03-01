3e2aa49fb381187893c5084085a10d40ea8db22a
sayali salokhe <sayalisalokhe895@gmail.com>
2023-03-01 15:13:34 +0530

    send  array assingment

---
+++ SayalliSalokhe/sayali_Salokhe_Array_Assingment/src/array_Assingment/EvenIndexPrint.java
@@ -0,0 +1,18 @@
+package array_Assingment;
+
+public class EvenIndexPrint {
+	public static void main(String[] args) {
+		int[] array= {10,30,50,89,22};
+		int index=0;
+	
+		while(index<array.length)
+		{
+	       System.out.println("Even No:"+array[index]);
+	      index= index+2;
+			
+		}
+		
+		
+	}
+
+}
