6a9989f3152a84c94d7fa298b115117df17a97f1
sayali salokhe <sayalisalokhe895@gmail.com>
2023-03-02 16:51:56 +0530

    send  switch case assingment

---
+++ SayalliSalokhe/sayaliSalokhe_Flow_Control/src/switch_Program/NumberMatch.java
@@ -0,0 +1,30 @@
+package switch_Program;
+
+public class NumberMatch {
+	public static void main(String[] args) {
+		int num = 110;
+		switch (num) {
+		case 10: {
+			System.out.println("numer is 10");
+			break;
+		}
+		case 70: {
+			System.out.println("numer is 70");
+			break;
+		}
+		case 170: {
+			System.out.println("numer is 170");
+			break;
+		}
+		case 99: {
+			System.out.println("numer is 99");
+			break;
+		}
+		default: {
+			System.out.println("pass wrong number");
+		}
+
+		}
+	}
+
+}
