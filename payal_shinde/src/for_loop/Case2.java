bbf5cd7aa3dc9b17cc8482fd08d884c2f3a0e527
payal shinde <payalshinde9696@gmail.com>
2023-03-03 23:09:35 +0530

    for loop

---
+++ payal_shinde/src/for_loop/Case2.java
@@ -0,0 +1,30 @@
+package for_loop;
+
+public class Case2 {
+
+		public static void main(String[] args) {
+			
+			int[] arr = {10,20,30,40,50};
+			
+			int index = 0;
+			
+			while(index < arr.length) {
+				System.out.println(arr[index]);
+				index++;
+			}
+			System.out.println("==========================");
+			//=======================================
+			
+				
+			int[] arr1 = {100,200,300,400,500};
+			
+			for(int i =0 ; i< arr1.length ; i++) {
+				System.out.println(arr1[i]);
+			}
+			
+			
+			
+		}
+	}
+
+
