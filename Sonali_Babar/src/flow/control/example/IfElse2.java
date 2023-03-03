102539422f3a09d8e8d2e9be2e9f15e090af8290
babarsonali821@gmail.com <babarsonali821@gmail.com>
2023-03-03 21:53:49 +0530

    data

---
+++ Sonali_Babar/src/flow/control/example/IfElse2.java
@@ -0,0 +1,27 @@
+package flow.control.example;
+
+public class IfElse2
+{
+	public static void main(String[] args) 
+	{
+		int a=10;
+		if(a==100)
+		  System.out.println("true part block");
+		else
+			System.out.println("false part block");
+		
+		 System.out.println("End of program");
+		
+		 int b=100;
+		 if(b==99);
+		 	System.out.println("true part");
+		 	System.out.println("End of program");
+			 
+			 
+			
+		
+		
+		
+	}
+
+}
