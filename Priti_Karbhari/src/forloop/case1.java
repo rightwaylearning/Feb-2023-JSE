0b3d2f359a27b746f9841b49d277331f5cea1dd3
priti <pkarbhari0399@gmail.com>
2023-03-03 21:35:25 +0530

    practice

---
+++ Priti_Karbhari/src/forloop/case1.java
@@ -0,0 +1,20 @@
+package forloop;
+
+public class case1 {
+	public static void main(String[] args) {
+		//String[] arr = {"abc","b","c","d"};
+		
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 5; j++) {
+				System.out.println("hello");	
+			}
+			//System.out.println("hello");
+		} 
+			
+		}
+		
+		
+		
+	}
+ 
+
