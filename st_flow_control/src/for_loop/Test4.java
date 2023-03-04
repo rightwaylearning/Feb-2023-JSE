261750651a2a836a4ff65644bd6966beb530a64e
Swapnali <swap4399@gmail.com>
2023-03-04 07:16:15 +0530

    programs

---
+++ st_flow_control/src/for_loop/Test4.java
@@ -0,0 +1,16 @@
+package for_loop;
+
+public class Test4 {
+	public static void main(String[] args) {
+		
+		for(int i = 1; i<=5; i++) {
+			
+			for(int j = 1; j<=5; j++) {
+				System.out.println("Hello");
+				
+			}		
+		}
+		
+	}
+
+}
