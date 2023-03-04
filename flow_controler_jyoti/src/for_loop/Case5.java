63943506643ad24ca4c867056606e87179c17830
Jyoti Kendre <jyotikendre1999@gmail.com>
2023-03-04 17:01:25 +0530

    Java Data

---
+++ flow_controler_jyoti/src/for_loop/Case5.java
@@ -0,0 +1,18 @@
+package for_loop;
+
+public class Case5 {
+	public static void main(String[] args) {
+		
+		int [] arr1 = {100, 200, 300, 400, 500};
+		int i = 0;
+		
+		for(; true ;) {
+		System.out.println(arr1[i]);
+		i++;
+		}
+		
+	
+		
+	}
+
+}
