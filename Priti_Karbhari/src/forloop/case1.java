7bd7bb5d3d5b7b2b25b1db892e6bf4895b07a4f0
priti <pkarbhari0399@gmail.com>
2023-03-04 13:37:24 +0530

    practice

--- Priti_Karbhari/src/forloop/case1.java
+++ Priti_Karbhari/src/forloop/case1.java
@@ -2,18 +2,25 @@ package forloop;
 
 public class case1 {
 	public static void main(String[] args) {
-		String[] arr = {"abc","b","c","d"};
+		//String[] arr = {"abc","b","c","d"};
 		
-		for (int  i = 0;  i < arr.length; i = i+2) {
+		for (int  i = 1;  i <=3; i++) {
 			
 		
-			//for (int j = 1; j <= 5; j++) {
-				System.out.println(i);	
+			for (int j = 1; j <= 3; j++) {
+				if(i != j) {
+				System.out.println("("+i+" "+j+")");	
+				//break;
+			}else {
+				continue;
 			}
-			//System.out.println("hello");
+			System.out.println("##");
 		} 
+			//break;
 			
 		}
+}
+}
 		
 		
 		
