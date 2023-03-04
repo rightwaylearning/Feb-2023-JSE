7c147c1ac2d71986e2a0160dc7e688262052fe0f
priti <pkarbhari0399@gmail.com>
2023-03-04 14:42:53 +0530

    practice

---
+++ Priti_Karbhari/src/do_whileloop/Case2.java
@@ -0,0 +1,14 @@
+package do_whileloop;
+
+public class Case2 {
+	
+	public static void main(String[] args) {
+		
+		int i = 4;
+				
+				do {
+					System.out.println("hello");
+				}while(i<=4);
+	}
+
+}
