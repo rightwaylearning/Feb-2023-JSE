f67e65c0d4916086fd048482773aa11812a6353d
Akshay_Zarodiya <arz30121998@gmail.com>
2023-03-06 01:05:58 +0530

    this is my Student array pg

--- arz_java_fundamental/src/array/examples/Student.java
+++ arz_java_fundamental/src/array/examples/Student.java
@@ -7,28 +7,31 @@ public class Student {
 
 	public Student() {}
 
+	
 	public Student(int rollNumber, String name) {
 		super();
 		this.rollNumber = rollNumber;
 		this.name = name;
 	}
 
+
 	public int getRollNumber() {
 		return rollNumber;
 	}
+
 
 	public void setRollNumber(int rollNumber) {
 		this.rollNumber = rollNumber;
 	}
 
+
 	public String getName() {
 		return name;
 	}
 
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
-        
+     
 }
