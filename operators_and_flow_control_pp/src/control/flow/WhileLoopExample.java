732e627aea8ad3237aaceb5fd2e334f0adefd0d3
Your Name <you@example.com>
2023-03-05 19:00:29 +0530

    create a class

---
+++ operators_and_flow_control_pp/src/control/flow/WhileLoopExample.java
@@ -0,0 +1,22 @@
+package control.flow;
+
+public class WhileLoopExample {
+	
+	public static void main(String[] args) {
+		
+		int i=1;
+		
+		while(WhileLoopExample.validateNumber(i)) {
+			System.out.println(i);
+			i++;
+			
+		}	
+	}
+	public static boolean validateNumber(int i){
+		 boolean r=  i<=10;
+		 return r;
+		 
+			
+		}
+
+}
