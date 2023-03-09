e3a55a422dd993f91f460845a10cd20f7dd014d1
Jyoti Kendre <jyotikendre1999@gmail.com>
2023-03-09 22:34:22 +0530

    Declaration data

---
+++ declaration_and_modifiers/src/do_opt1/EnteryPoint.java
@@ -0,0 +1,23 @@
+package do_opt1;
+
+import package_3.Two;
+import java.util.Date;
+import package_3.IamTwo;
+
+public class EnteryPoint {
+	
+	public static void main(String[] args) {
+		
+		Two obj = new Two();
+		
+		obj.showMe();
+		
+		IamTwo obj2 = new IamTwo();
+		obj2.ShowMe();
+		
+		Date d1 = new Date();
+		
+
+	}
+
+}
---
+++ declaration_and_modifiers/src/package_3/Two.java
@@ -0,0 +1,10 @@
+package package_3;
+
+public class Two {
+	
+	public void showMe() {
+		
+		System.out.println("I am showMe method of Two class");
+	}
+
+}
