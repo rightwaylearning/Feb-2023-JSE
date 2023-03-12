515bc227b0c60b935a3195daa26cb479e51970cc
Rohit <rohitpandhare9@gmail.com>
2023-03-12 12:29:34 +0530

    inline import

--- rp_practice_session/src/practice_program2/Case2.java
+++ rp_practice_session/src/practice_program2/Case2.java
@@ -6,8 +6,11 @@ import rp_practice.programs.Case1;
 import static rp_practice.programs.Case1.m1;
 import static rp_practice.programs.Case1.name;
 
+
 //aapn jr static property import karaych aahe tr direct lihu shaktoh but 
 //vari static lihav lagel 
+
+
 
 public class Case2 {
 	
@@ -24,6 +27,14 @@ public class Case2 {
 		System.out.println(name);
 		
 		m1();
+
+//	yala mhnatat inline import karan class name same aahet different package aasle tari
+//		tya mule yala inline import karav lagte
+		practice.Case1 c1 =new practice.Case1();
+		
+		c1.name = "name";
+		System.out.println(c1.name);
+		
 	}
 
 }
