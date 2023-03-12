e403842508528a0168f7ebcd9e21cf5f0158f64f
Rohit <rohitpandhare9@gmail.com>
2023-03-12 12:25:03 +0530

    static import

--- rp_practice_session/src/practice_program2/Case2.java
+++ rp_practice_session/src/practice_program2/Case2.java
@@ -3,6 +3,12 @@ package practice_program2;
 
 import rp_practice.programs.Case1;
 
+import static rp_practice.programs.Case1.m1;
+import static rp_practice.programs.Case1.name;
+
+//aapn jr static property import karaych aahe tr direct lihu shaktoh but 
+//vari static lihav lagel 
+
 public class Case2 {
 	
 	public static void main(String[] args) {
@@ -11,9 +17,13 @@ public class Case2 {
 		
 		c.setMessage("hello");
 		
-		Case1.setName("this is name");
+		Case1.setname("this is name");
 		
-		System.out.println(Case1.Name);
+		Case1.name="hello";
+		
+		System.out.println(name);
+		
+		m1();
 	}
 
 }
--- rp_practice_session/src/rp_practice/programs/Case1.java
+++ rp_practice_session/src/rp_practice/programs/Case1.java
@@ -3,16 +3,20 @@ package rp_practice.programs;
 public class Case1 {
 	
 	public String message;
-	public static String Name;
+	static public String name;
 
 
 //	this is constructor in that aapn object properties initialize karu shaktoh
 	public Case1(){
 		this.message = "hello2 message";
-		Case1.Name = "feff";
+		Case1.name = "feff";
 		
 	}
 	
+	public static void m1() {
+		
+		System.out.println("this is static method");
+	}
 	
 	public Case1(String message) {
 		super();
@@ -30,13 +34,13 @@ public class Case1 {
 	}
 
 
-	public static String getName() {
-		return Name;
+	public static String getname() {
+		return name;
 	}
 
 
-	public static void setName(String name) {
-		Name = name;
+	public static void setname(String name) {
+		Case1.name = name;
 	}
 
 
