a5b10da8660d7aa9fe284937d8f23764acbd4f2e
sayali salokhe <sayalisalokhe895@gmail.com>
2023-03-12 20:11:06 +0530

    send pakage declration programm

---
+++ SayalliSalokhe/sayaliSalokhe_Pakage_Declaration/src/opration/EnteryPoint.java
@@ -0,0 +1,15 @@
+package opration;
+
+import package_One.*;
+
+public class EnteryPoint {
+	public static void main(String[] args) {
+		One one=new One();
+		one.showme();
+		package_two.One one2=new package_two.One();
+		one2.showother();
+		Iam_One iam_One=new Iam_One();
+		iam_One.showme();
+	}
+
+}
