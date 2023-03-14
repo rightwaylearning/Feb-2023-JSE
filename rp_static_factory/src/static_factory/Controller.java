8df08bf142662931271583bf76f648227ed90555
Rohit <rohitpandhare9@gmail.com>
2023-03-14 11:50:33 +0530

    new static factory

--- rp_static_factory/src/static_factory/Controller.java
+++ rp_static_factory/src/static_factory/Controller.java
@@ -4,11 +4,20 @@ public class Controller {
 
 	public static void main(String[] args) {
 	      
-		C c = new C();
-		B b = new B(c);
-		A a =new A(b);
+//		C c = new C();
+//		B b = new B(c);
+//		A a =new A(b);
+//		
+//	   System.out.println(a.messageA);
 		
-	   System.out.println(a.messageA);
+		
+		System.out.println(StaticWayToCall.m1());
+		
+//		pahile ya madhe static factory ch object return hoat aahe tya mule aasa aala aahe
+
+		A a1 =StaticWayToCall.m1();
+		
+		System.out.println(a1.messageA);
 		
 		
 	}
