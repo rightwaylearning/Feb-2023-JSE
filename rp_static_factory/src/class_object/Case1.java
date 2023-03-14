745148162c5f5f6bf1a5df5d5bd7e54729e5b700
Rohit <rohitpandhare9@gmail.com>
2023-03-14 12:07:26 +0530

    object

---
+++ rp_static_factory/src/class_object/Case1.java
@@ -0,0 +1,13 @@
+package class_object;
+
+public class Case1 {
+	
+	void m1() {
+		System.out.println("hello this is method1");
+	}
+
+	void m2() {
+		System.out.println("hello this is method2");
+	}
+	
+}
