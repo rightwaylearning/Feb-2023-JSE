45d789c20f294c5e25d5503cde9b3fa49cefb255
Rohit <rohitpandhare9@gmail.com>
2023-03-16 13:45:39 +0530

    Factory method only object return

--- rp_java_is_pure_oop_lang/src/wrapper/classes/primitve/type/Input_output.java
+++ rp_java_is_pure_oop_lang/src/wrapper/classes/primitve/type/Input_output.java
@@ -3,9 +3,10 @@ package wrapper.classes.primitve.type;
 public class Input_output {
 
 	
-	public static String m1(Object c) {
+	public static String m1(String c) {
 		
-		String str = (String)c ;
+		
+		String str = new String(c) ;
 	
 		return str;
 	}
