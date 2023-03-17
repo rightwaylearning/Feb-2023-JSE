adb17218369e2a5dd54eed1731a0f8a4436acb0a
Rohit <rohitpandhare9@gmail.com>
2023-03-17 11:27:04 +0530

    abstrac class

---
+++ rp_java_is_pure_oop_lang/src/abstract_class_for_abstraction/Child.java
@@ -0,0 +1,10 @@
+package abstract_class_for_abstraction;
+
+public class Child extends Parent {
+	
+	void buildHome() {
+		
+		System.out.println("build home pn zal");
+	}
+
+}
---
+++ rp_java_is_pure_oop_lang/src/abstract_class_for_abstraction/Controller.java
@@ -0,0 +1,16 @@
+package abstract_class_for_abstraction;
+
+public class Controller {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		Parent p = new Child();
+	      p.buildHome();
+	      p.property();
+		
+		
+	}
+
+}
