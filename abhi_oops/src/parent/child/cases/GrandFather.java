7cb96ffff51449b49c2575c85ca827a100bf4667
abhi982355 <rautabhishek2002@gmail.com>
2023-03-19 12:21:05 +0530

    Family reln

---
+++ abhi_oops/src/parent/child/cases/GrandFather.java
@@ -0,0 +1,16 @@
+package parent.child.cases;
+
+abstract public class GrandFather {
+
+	void business() {
+
+		System.out.println("LIC Agent");
+	}
+
+	void income() {
+
+		System.out.println("Rs.25000 / month");
+	}
+
+	abstract void grandMother();
+}
