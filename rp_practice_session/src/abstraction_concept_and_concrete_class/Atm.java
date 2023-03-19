78fe5fb7f0d056a40772d071dce51bc9eb143ccc
Rohit <rohitpandhare9@gmail.com>
2023-03-19 12:32:49 +0530

    practice

---
+++ rp_practice_session/src/abstraction_concept_and_concrete_class/Atm.java
@@ -0,0 +1,19 @@
+package abstraction_concept_and_concrete_class;
+
+public class Atm extends User {
+	
+	void passWordpin() {
+		
+		System.out.println("977473");
+		
+	}
+	
+	void money() {
+		
+		System.out.println("money");
+		
+	}
+	
+	
+
+}
---
+++ rp_practice_session/src/abstraction_concept_and_concrete_class/User.java
@@ -0,0 +1,10 @@
+package abstraction_concept_and_concrete_class;
+
+abstract public class User {
+	
+    abstract void passWordpin() ;
+    
+    abstract void money() ;
+    
+
+}
