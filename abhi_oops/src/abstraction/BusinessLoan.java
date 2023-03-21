c9c9f01b0826ddbd318ca1458d1d599a31625873
abhi982355 <rautabhishek2002@gmail.com>
2023-03-21 12:03:14 +0530

    abstraction

---
+++ abhi_oops/src/abstraction/BusinessLoan.java
@@ -0,0 +1,10 @@
+package abstraction;
+
+public class BusinessLoan extends Loan {
+
+	public void myLoan() {
+		
+		System.out.println("doc: IT return , Income Cetificate ,Cibil Score , Stock details ,etc");
+	}
+
+}
