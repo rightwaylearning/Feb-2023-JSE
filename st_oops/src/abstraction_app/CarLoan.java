3ca48a8d125a8273c87366b80af0ffd1eba02723
Swapnali <swap4399@gmail.com>
2023-03-21 17:06:10 +0530

    programs

---
+++ st_oops/src/abstraction_app/CarLoan.java
@@ -0,0 +1,11 @@
+package abstraction_app;
+
+public class CarLoan extends Loan {
+	
+	void myLoan() {
+		System.out.println("RTO document, Application Form");
+		System.out.println("Income Doc:- pay slip, bank ststement etc.");
+    	
+	}
+
+}
