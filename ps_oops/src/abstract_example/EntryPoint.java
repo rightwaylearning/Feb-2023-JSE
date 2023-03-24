404bd2a3bfb4a2ce13d071f26a1844c5b7e939ab
payal shinde <payalshinde9696@gmail.com>
2023-03-24 22:52:23 +0530

    oops

---
+++ ps_oops/src/abstract_example/EntryPoint.java
@@ -0,0 +1,16 @@
+package abstract_example;
+
+public class EntryPoint {
+       public static void main(String[] args) {
+		
+    	   AA a;
+    	   
+    	   AA a1 = new AA();
+    	   
+    	   //-------------
+    	   BB b;
+    	   
+    	//   BB b1 = new BB(); // Cannot instantiate the type BB
+    	   
+	}
+}
---
+++ ps_oops/src/reusability/Student1.java
@@ -0,0 +1,9 @@
+package reusability;
+
+public class Student1 extends Maths {
+
+	void doOpt(int a, int b) {
+		int r = product(a,b);
+		System.out.println("IS-A-Reln  "+r);
+	}
+}
