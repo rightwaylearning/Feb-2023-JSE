f1c32e37944badf962f511923cf4f7fb8b86d010
Rohit <rohitpandhare9@gmail.com>
2023-03-25 15:44:14 +0530

    practice data file

--- rp_reusability_HAS-A-reln/src/has_a_reln_aggregation/Aggregation.java
+++ rp_reusability_HAS-A-reln/src/has_a_reln_aggregation/Aggregation.java
@@ -2,4 +2,10 @@ package has_a_reln_aggregation;
 
 public class Aggregation {
 
+	B b ;
+	
+	C c ;
+	
+	
+	
 }
--- rp_reusability_HAS-A-reln/src/has_a_reln_aggregation/Controller.java
+++ rp_reusability_HAS-A-reln/src/has_a_reln_aggregation/Controller.java
@@ -1,5 +1,30 @@
 package has_a_reln_aggregation;
 
 public class Controller {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		Aggregation a = new Aggregation();
+		
+		             B b = new B();
+		             C c = new C();
+		
+		a.b =b;
+		a.c =c;
+		
+		
+		
+//		this is example of weak association means here if destroy container 
+//		object also still here exist Container objects here
+		
+//		if container object and contained objects having weak association is called
+//		aggregation
+		
+		
+		
+	}
 
 }
