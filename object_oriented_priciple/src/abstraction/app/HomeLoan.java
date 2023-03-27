9e863e13f266c7c92caa113185f0afc3519f13f2
sachin manjare <sachinmanjare93@gmail.com>
2023-03-27 21:13:58 +0530

    data

---
+++ object_oriented_priciple/src/abstraction/app/HomeLoan.java
@@ -0,0 +1,12 @@
+package abstraction.app;
+
+public  class HomeLoan extends Loan {
+	void myLoan() {
+		
+		System.out.println("doc of flat");
+		System.out.println("rigisteration doc");
+	}
+	
+	
+
+}
