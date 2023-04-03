5936d407d3dcd8faafc120d4dc4521fc174e2b47
sayali salokhe <sayalisalokhe895@gmail.com>
2023-04-03 12:24:44 +0530

    declaration and modifiers assingment

---
+++ SayalliSalokhe/sayaliSalokhe_declaration_and_Modifiers/src/sayaliSalokhe_declaration_and_Modifiers/A.java
@@ -0,0 +1,25 @@
+package sayaliSalokhe_declaration_and_Modifiers;
+
+public class A {
+	public static void main(String[] args) {
+		System.out.println("class A");
+	}
+
+}
+
+//Case1:The public type B must be defined in its own file
+/*public class B {
+	public static void main(String[] args) {
+		System.out.println("class A");
+	}
+}*/
+
+class C {
+	public static void main(String[] args) {
+		System.out.println("class C");
+	}
+
+}
+class D{
+	
+}
---
+++ SayalliSalokhe/sayaliSalokhe_oopsConcept/src/inheritance_Examle/TreeExample.java
@@ -0,0 +1,14 @@
+package inheritance_Examle;
+
+class M {
+}
+
+class D extends M {
+}
+
+class P extends M {
+}
+
+public class TreeExample {
+
+}
