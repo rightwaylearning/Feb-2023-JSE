8f4949e4f9de9e187c38d8395e12688d528266f0
sayali salokhe <sayalisalokhe895@gmail.com>
2023-04-08 00:19:22 +0530

    method overriding assingment

--- SayalliSalokhe/sayaliSalokhe_oopsConcept/src/overriding_Rule/Rule7_Abstract.java
+++ SayalliSalokhe/sayaliSalokhe_oopsConcept/src/overriding_Rule/Rule7_Abstract.java
@@ -1,5 +1,27 @@
 package overriding_Rule;
 
+class s { // non abstract
+	void m1() {
+	}
+
+}
+
+abstract class s1 extends s { // abstract
+	abstract void m1();
+
+}
+
+class s2 extends s1 {  //non-abstract
+
+	@Override
+	void m1() {
+
+	}
+}
+
 public class Rule7_Abstract {
+	public static void main(String[] args) {
+
+	}
 
 }
