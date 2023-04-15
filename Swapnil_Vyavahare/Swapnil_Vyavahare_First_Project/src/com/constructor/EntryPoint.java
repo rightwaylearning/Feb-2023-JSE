5f4c00b2471d8b21f01aaa0b11d794df9c7024d3
abc <swapnilplan@gmail.com>
2023-04-15 18:09:29 +0530

    data

---
+++ Swapnil_Vyavahare/Swapnil_Vyavahare_First_Project/src/com/constructor/EntryPoint.java
@@ -0,0 +1,12 @@
+package com.constructor;
+
+public class EntryPoint {
+
+	public static void main(String[] args) {
+
+		P p = new C();
+		p.m1(10);
+		
+
+	}
+}
---
+++ Swapnil_Vyavahare/Swapnil_Vyavahare_First_Project/src/com/constructor/P.java
@@ -0,0 +1,20 @@
+package com.constructor;
+
+public class P {
+
+	int a=100;
+	
+	public P() {
+		
+	}
+	
+	void m1(int a) {
+		this.a=a;
+		System.out.println("I am p");
+		
+	}
+
+	
+}
+
+
