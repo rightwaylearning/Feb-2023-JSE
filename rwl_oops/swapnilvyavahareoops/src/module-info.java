83b27b770cab2aa93fa47a3f8269c386fed2ee3f
SWAPNIL VYAVAHARE <swapnilvyavahare1999@gmail.com>
2023-04-27 23:14:00 +0530

    data

--- rwl_oops/swapnilvyavahareoops/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author Intel
- *
- */
-module swapnilvyavahareoops {
-}
--- rwl_oops/swapnilvyavahareoops/src/swapnilvyavahareoops/superthis/Entrypoint.java
+++
@@ -1,8 +0,0 @@
-package swapnilvyavahareoops.superthis;
-
-public class Entrypoint {
-
-	public static void main(String[] args) {
-		Constructorcall c= new Constructorcall(10,20);
-	}
-}
